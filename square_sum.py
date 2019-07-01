d3a066944e0517918b9261396c6210d38248f9e0
ShawnSXSong <52312125+ShawnSXSong@users.noreply.github.com>
2019-07-01 14:21:54 +0800

    Update square_sum.py

--- square_sum.py
+++ square_sum.py
@@ -2,13 +2,11 @@ def square_sum(a,b):
     """return the square sum of two arguments
         sample: square(a,b)"""
     a,b = int(a),int(b) #insure data type suit to this function
-    a = a**2
-    b = b**2
+    a,b = a**2,b**2
     c = a + b
     return c
 
-a = input("first num:")
-b = input("second num:")
+a,b = input("first num:"),input("second num:")
 c = square_sum(a,b)
 print(c)
-help(square_sum)
+#help(square_sum)
